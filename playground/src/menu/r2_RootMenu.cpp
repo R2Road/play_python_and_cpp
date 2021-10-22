e64899f46deeb5009fd56c17b2651fcd33f8cf0b
r2road <studior2road@gmail.com>
2021-10-22 21:09:58 +0900

    not need

--- playground/src/menu/r2_RootMenu.cpp
+++ playground/src/menu/r2_RootMenu.cpp
@@ -22,43 +22,8 @@ namespace r2
 		{
 			ret->AddChild( '1', lua_state_test::Basic::GetInstance() );
 
+
 			ret->AddLineFeed();
-
-
-			ret->AddSplit();
-
-
-			ret->AddChild(
-				32
-				, []()->const char* { return "All"; }
-				, []()->eTestResult
-				{
-					int input = 0;
-					bool process = true;
-					while( process )
-					{
-						input = _getch();
-						system( "cls" );
-
-						switch( input )
-						{
-						case '5':
-							//step::OpenLibs();
-							break;
-
-						case 27: // ESC
-							process = false;
-							break;
-						}
-
-						std::cout << r2::linefeed << "Press Any Key" << r2::linefeed;
-						_getch();
-						system( "cls" );
-					}
-
-					return eTestResult::RunTest;
-				}
-			);
 
 
 			ret->AddSplit();
