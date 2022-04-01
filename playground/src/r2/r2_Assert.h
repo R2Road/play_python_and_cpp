5d4cd81d95a9845e0b828710045127341b0179b6
r2road <studior2road@gmail.com>
2022-04-02 00:04:19 +0900

    copy from "fmod_playground"

--- playground/src/r2/r2_Assert.h
+++ playground/src/r2/r2_Assert.h
@@ -1,17 +1,34 @@
 #pragma once
 
 #ifndef R2_ASSERT_H
-	#define R2_ASSERT_H
+#define R2_ASSERT_H
 
-	#include <cassert>
-	#include <iostream>
 
-	#define R2_DEBUG 1
 
-	#if !defined( R2_DEBUG ) || R2_DEBUG == 0
-		#define R2ASSERT( cond, msg )     do {} while ( 0 )
-	#else
-		#define R2ASSERT( cond, msg )     do { std::cerr << msg; assert( cond ); } while ( 0 )
-	#endif
+#include <cassert>
+#include <iostream>
+
+#define R2_DEBUG 1
+
+#if !defined( R2_DEBUG ) || R2_DEBUG == 0
+
+#define R2ASSERT_2( cond, msg )     do {} while ( 0 )
+
+#else
+
+#define	R2ASSERT( cond, message )																											\
+do {																																		\
+	if( !( cond ) )																															\
+	{																																		\
+		printf( "+ R2ASSERT" "\nMessage : %s" "\nFile : %s" "\nLine : %d" "\nFunction : %s", message, __FILE__, __LINE__, __FUNCTION__ );	\
+		__debugbreak();																														\
+		throw std::runtime_error( message );																								\
+	}																																		\
+} while( false )
+
+#endif
+
+
+
 
 #endif
